ca136b643fddef8c773640cb32f423915ec79bd2
Elvis Nobrega <elvismdnin@gmail.com>
2021-01-07 20:53:11 -0300

    Primeira versão completa da árvore de decisão baseada nos dados histopatológicos

--- prot2vec/DecisionTreeHist.py
+++ prot2vec/DecisionTreeHist.py
@@ -1,59 +1,20 @@
 import pandas as pd
 import numpy as np
-from sklearn import tree
+import re
 import graphviz
+from sklearn import tree
 
-vectorscomp = dict()
-vectorsprot = dict()
-dotprot = dict()
-BC2 = pd.read_excel('BC2r.xlsx')
-BC2WE = pd.read_csv('BC2_word_embeddings.csv')
+Hist = pd.read_excel('Data/hist_data.ods')
 
+Hist = Hist.dropna()
 
-def one_hot_encoding_of_length(length):
-    out = []
-    values = list(dotprot.keys())
-    labels = []
-    for i in range(0, length):
-        vec = np.zeros(length)
-        vec[i] = 1.0
-        out.append(vec)
-        labels.append(values[i])
-    return out, labels
+Hist['gender'] = Hist['gender'].map({'Male': 0, 'Female': 1})
+X = Hist.drop('id', axis=1).drop('dead by melanoma', axis=1)
+Y = Hist['dead by melanoma']
 
+hist_tree = tree.DecisionTreeClassifier(max_depth=5)
+hist_tree.fit(X, Y)
 
-def gen_vectors_complete(protein, vector):
-    vectorscomp[protein] = np.array(vector)
-
-
-def gen_vectors_prot(protein, vector):
-    vectorsprot[protein] = np.array(vector)
-
-
-proteins = BC2['Entry'].tolist()
-vectorsdp = BC2WE[BC2WE['word'].isin(proteins)]
-vectorsdp = vectorsdp.fillna(0.0)
-
-BC2WE.fillna(0.0).apply(lambda row: gen_vectors_complete(row['word'], row[1:]), raw=True, axis=1)
-vectorsdp.apply(lambda row: gen_vectors_prot(row['word'], row[1:]), raw=True, axis=1)
-
-#print(BC2WE.loc[8742]) #"Angiogenic'
-#print(vectorscomp['ANGIOGENIC'])
-
-dotprot_len = 0
-for prot, vec in vectorsprot.items():
-    dotprot[prot] = vec.dot(vectorscomp['ANGIOGENIC'])
-    dotprot_len += 1
-
-X, labels = one_hot_encoding_of_length(dotprot_len)
-Y = list(dotprot.values())
-
-angiogenesis_tree = tree.DecisionTreeRegressor()
-angiogenesis_tree.fit(X, Y)
-
-
-dot_data = tree.export_graphviz(angiogenesis_tree, out_file=None, feature_names=labels)
+dot_data = tree.export_graphviz(hist_tree, out_file=None, feature_names=X.columns, class_names=["Vivo", "Morto"])
 graph = graphviz.Source(dot_data)
-graph.render("Angiogenesis")
-
-print(sorted(dotprot.items(), key=lambda x: x[1]))
+graph.render("MortePorMelanoma")
